a0ce655d128c34bbb21a4fdaefa8d5e17b72f8e9
zythum <zythum02@gmail.com>
2015-05-18 22:10:04 +0800

    ps4 video canplaytype说支持m3u8 但是结果播不了。什么鬼！

--- src/canPlayM3U8.js
+++ src/canPlayM3U8.js
@@ -2,4 +2,5 @@
  *  返回浏览器是否支持m3u8格式的视频播放。
  *  目前chrome,firefox只支持mp4
  */
-module.exports = !!document.createElement('video').canPlayType('application/x-mpegURL')
+module.exports = !!document.createElement('video').canPlayType('application/x-mpegURL') && 
+	navigator.userAgent.indexOf('PlayStation') === -1
