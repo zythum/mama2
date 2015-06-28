b2c2e73bf14ad24c6e1dbc02594d58fea61f6562
情迷海龟pizza <zhshipu@gmail.com>
2015-06-28 16:56:15 +0800

    增加对芒果TV的支持

--- src/seekers.js
+++ src/seekers.js
@@ -3,6 +3,7 @@ module.exports = [
 	require('./seeker_youku'),
 	require('./seeker_tudou'),
     require('./seeker_iqiyi'),
-    require('./seeker_sohu')
+    require('./seeker_sohu'),
+    require('./seeker_hunantv')
 	// ,require('./seeker_example')
 ]
