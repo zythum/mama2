e10980c390ab76fbb572922a70055c08b727ea63
feiyulike <1409628@qq.com>
2016-04-02 13:49:05 +0800

    修复切换为flash按钮功能

--- dest/index_no_uglify.js
+++ dest/index_no_uglify.js
@@ -126,6 +126,7 @@ if (matched === undefined) {
 }
 
 }
+
 },{"./createElement":4,"./flashBlocker":5,"./log":10,"./mamaKey":11,"./player":13,"./purl":14,"./seeker_flvsp":20,"./seekers":25}],2:[function(require,module,exports){
 /*  \uff03function ajax#
  *  < {
@@ -745,11 +746,38 @@ module.exports = queryString
  *  @HugoLee 2016.4.1
  */
 
+var flashText = '<div style="text-shadow:0 0 2px #eee;letter-spacing:-1px;background:#eee;font-weight:bold;padding:0;font-family:arial,sans-serif;font-size:30px;color:#ccc;width:152px;height:52px;border:4px solid #ccc;border-radius:12px;position:absolute;top:50%;left:50%;margin:-30px 0 0 -80px;text-align:center;line-height:52px;">Flash</div>';
+
+var iframe=document.getElementsByTagName("iframe")[0]
+var style = [
+    '',
+    'width:100%',
+    'height:100%',
+    'cursor:pointer',
+    'background-color:#eee',     
+    ''
+  ]
+var click2ShowFlash=function(){
+	document.body.appendChild(iframe)
+}
+var blockframe=function(){
+	//var iframeSrc=iframe.src
+	//var iframeStyle=iframe.style
+	document.body.removeChild(iframe)
+	var placeHolder = document.createElement('div');
+	  placeHolder.setAttribute('title', '&#x70B9;&#x6211;&#x8FD8;&#x539F;Flash');
+	  placeHolder.addEventListener('click', click2ShowFlash, false);
+	  placeHolder.innerHTML = flashText;	
+	  placeHolder.style.cssText += style.join(';');
+	document.body.appendChild(placeHolder)
+//console.log(iframeStyle)
+}
 exports.match = function (url) {
   return url.attr('source').indexOf('play.html?pickcode=') >= 0 || /^http\:\/\/115.com/.test(url.attr('source'))
 }
 exports.getVideos = function (url, callback) {
-	document.body.removeChild(document.getElementsByTagName("iframe")[0])
+	blockframe()
+	//document.body.removeChild(document.getElementsByTagName("iframe")[0])
   callback([["115\u8001\u53f8\u673a", "http://115.com/?ct=download&ac=video&pickcode="+url.data.param.query.pickcode]])
 }
 
