15ed87adf4f4cf3c39de9efd9249043b3e6dcf57
lycycycycyc <lyc@lycdeMacBook-Pro.local>
2016-04-12 03:47:18 +0800

    add serverURL setting

--- extension/mama2.safariextension/script.js
+++ extension/mama2.safariextension/script.js
@@ -1,5 +1,5 @@
-var url = 'http://zythum.sinaapp.com/mama2/dest/index.js'
 safari.self.addEventListener("message", function(theMessageEvent){
+	var url = theMessageEvent.message;
 	if(theMessageEvent.name == 'MAMA2'){
 		if(window === window.top){
 			(function(s){s=document.body.appendChild(document.createElement('script'));s.src=url+'?ts='+Date.now();s.charset='UTF-8';}())
