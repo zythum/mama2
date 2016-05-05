6a245907ca49e5cea3e4f404a9b0d04b5f629c7b
Downpooooour <mhcgrq@gmail.com>
2016-05-06 02:34:31 +0800

    加入对音悦台的支持

--- src/seekers.js
+++ src/seekers.js
@@ -6,6 +6,7 @@ module.exports = [
   require('./seeker_hunantv'),
   require('./seeker_baidupan'),
   require('./seeker_91porn'),
-  require('./seeker_pandatv')
+  require('./seeker_pandatv'),
+  require('./seeker_yinyuetai')
   // ,require('./seeker_example')
 ]
