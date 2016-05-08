a060215c4aef917caba325fed69d3f5e3863903f
Downpooooour <mhcgrq@gmail.com>
2016-05-09 01:16:57 +0800

    更新，已经可以正常播放

--- src/seeker_yinyuetai.js
+++ src/seeker_yinyuetai.js
@@ -36,12 +36,15 @@ exports.getVideos = function (url, callback) {
 			var video = res.videoInfo.coreVideoInfo.videoUrlModels;
 			var mode = ['普清', '高清', '超清', '会员'];
 			for (var i = 0; i < video.length; i++) {
-				data.push([mode[i], video[i].videoUrl]);
+				var index = video[i].videoUrl.search(/(flv|mp4)/) + 3;
+				data.push([mode[i], video[i].videoUrl.slice(0, index)]);
 			}
 			console.log(data);
+			callback(data);
 		}
 	});
-
-	callback(data);
 }
 
+//	少数情况下会出现如下错误，目前不清楚是怎么回事，有时候出现这种情况后，重新打开一遍就又可以播放了
+// GET http://120.192.249.220:9090/data4/1/c/3a/c/a38df997fecc82d251482b4bcf6c3ac1/hc.yinyuetai.com/CDD8015436C4EAFED49290FE1AA16449.flv?type=data 404 (Not Found)
+// index.js:480 Uncaught (in promise) DOMException: The element has no supported sources.
