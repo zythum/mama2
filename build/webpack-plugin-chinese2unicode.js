413e31664409a0a9d9b8100da263ce421eb86e03
zythum <zythum02@gmail.com>
2016-06-12 23:15:11 +0800

    修改c2u插件，改成webpack内部输出

--- build/webpack-plugin-chinese2unicode.js
+++ build/webpack-plugin-chinese2unicode.js
@@ -2,7 +2,7 @@
 //避免在一些不是utf-8的网站上文字显示乱码
 
 function toUnicode (s) {
-  return s.replace(/([\u4E00-\u9FA5]|[\uFE30-\uFFA0])/g, function() {
+  return s.replace(/([\u4E00-\u9FA5]|[\uFE30-\uFFA0])/g, function () {
     return '\\u' + RegExp['$1'].charCodeAt(0).toString(16)
   })
 }
@@ -11,13 +11,13 @@ function Chinese2unicodePlugin () {}
 Chinese2unicodePlugin.prototype.apply = function (compiler) {
   compiler.plugin('emit', function (compilation, callback) {
     for (var assetName in compilation.assets) {
-      var asset = compilation.assets[assetName]
-      compilation.assets[assetName] = (function (source) {
+      compilation.assets[assetName] = (function (asset) {
+        var source = toUnicode(asset.source())
         return {
           source: function () { return source },
           size: function () { return source.length }
         }
-      }(toUnicode(asset.source())))
+      }(compilation.assets[assetName]))
     }
     callback()
   })
